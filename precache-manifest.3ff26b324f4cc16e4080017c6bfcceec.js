a32e1e6bc8cf9109508beb7b00227b00121dfd61
Lev Vasilyev <torikowashi@gmail.com>
2020-06-07 09:42:48 +0300

    Updates

---
+++ precache-manifest.3ff26b324f4cc16e4080017c6bfcceec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3bdf8c5632e9774b2ec4b45be5d08b",
+    "url": "/Vilisov-BBD/index.html"
+  },
+  {
+    "revision": "0b35f4a48f854a1dd526",
+    "url": "/Vilisov-BBD/static/css/main.752df1a5.chunk.css"
+  },
+  {
+    "revision": "40f26b65c04a154da810",
+    "url": "/Vilisov-BBD/static/js/2.7ace357a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Vilisov-BBD/static/js/2.7ace357a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b35f4a48f854a1dd526",
+    "url": "/Vilisov-BBD/static/js/main.4eba740b.chunk.js"
+  },
+  {
+    "revision": "a4811a330e66bb0cb865",
+    "url": "/Vilisov-BBD/static/js/runtime-main.cb668a5e.js"
+  },
+  {
+    "revision": "f3152ab191c5a7dedad016c9b16308f3",
+    "url": "/Vilisov-BBD/static/media/Astronaut.f3152ab1.glb"
+  },
+  {
+    "revision": "3b24078346a6ae8b9caeca47834fcea9",
+    "url": "/Vilisov-BBD/static/media/MonumentExtended-Ultrabold.3b240783.otf"
+  },
+  {
+    "revision": "15a2a887d731830f629906cd581e3b3c",
+    "url": "/Vilisov-BBD/static/media/RF-Dewi-Expanded-Semibold.15a2a887.otf"
+  },
+  {
+    "revision": "3d7d57933a3689b6de7b75b14e02045c",
+    "url": "/Vilisov-BBD/static/media/blackgrid.3d7d5793.jpg"
+  },
+  {
+    "revision": "e8a0d624918e10153d03e580a98571a8",
+    "url": "/Vilisov-BBD/static/media/coin.e8a0d624.glb"
+  },
+  {
+    "revision": "466139342aeb36a9a65ec1cc031613df",
+    "url": "/Vilisov-BBD/static/media/tree.46613934.glb"
+  }
+]);
